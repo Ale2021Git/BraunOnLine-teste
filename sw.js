d1732b14cc8c6644b1fa4d9457988ceea839cbcb
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-06-05 13:36:36 -0300

    Increment cache version to v4.0
    Updated cache name to version 4.0 for new changes.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome do cache - Incremente a versão sempre que fizer mudanças grandes
-const CACHE_NAME = 'braun-v3.3';
+const CACHE_NAME = 'braun-v4.0';
 
 // Lista de arquivos essenciais (Incluindo os novos screenshots do manifesto)
 const assets = [
